807855c2730e652336738aac73b591dc3e2ab77b
Artiom Baloian <artiom.baloian@gmail.com>
2023-07-17 17:18:56 -0400

    Provided 2023 and 2024 calendar for NYSE

--- markets/nyse.ts
+++ markets/nyse.ts
@@ -1,34 +1,27 @@
 // All NYSE markets observe U.S. holidays as listed below for 2020,
 // 2021, and 2022.
 export const nyseHolidays = {
-  2020: {
-    January: [1, 20],
-    February: [17],
-    April: [10],
-    May: [25],
-    July: [3],
-    September: [7],
-    November: [26],
+  2023: {
+    January: [16],
+    February: [20],
+    April: [7],
+    May: [29],
+    June: [19],
+    July: [4],
+    September: [4],
+    November: [23],
     December: [25]
   },
-  2021: {
-    January: [1, 18],
-    February: [15],
-    April: [2],
-    May: [31],
-    July: [5],
-    September: [6],
-    November: [25],
-    December: [24]
-  },
-  2022: {
-    January: [17],
-    February: [21],
-    April: [15],
-    May: [30],
+  2024: {
+    January: [15],
+    February: [19],
+    March: [29],
+    April: [7],
+    May: [27],
+    June: [19],
     July: [4],
-    September: [5],
-    November: [24],
-    December: [26]
+    September: [2],
+    November: [28],
+    December: [25]
   }
 };
