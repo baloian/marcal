67447605e8fd53b66eab3fefb88bcc633385e7af
Artiom Baloian <artiom.baloian@gmail.com>
2024-05-22 22:29:03 -0400

    Fixed method

--- tests.ts
+++ tests.ts
@@ -2,7 +2,7 @@ import { MarCal, MarCalType } from './marcal';
 
 const marCal: MarCalType = new MarCal();
 
-const is_nyse_open = marCal.isMarketOpen();
+const is_nyse_open = marCal.marketOpen();
 if (is_nyse_open) {
   console.log('NYSE is open')
 } else {
