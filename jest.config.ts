cb0f0bbc27e7732aa3c2481b2b7ca54b727bda01
Artiom Baloian <artiom.baloian@gmail.com>
2024-11-04 11:13:27 -0500

    Added jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+};
+
+export default config;
