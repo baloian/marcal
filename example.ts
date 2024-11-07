ffb32afc4e846a9322d8679523efa1bf799b7149
Artiom Baloian <artiom.baloian@gmail.com>
2024-11-07 13:51:19 -0500

    Updated example

--- example.ts
+++ example.ts
@@ -2,9 +2,17 @@ import { MarCal, MarCalType } from './marcal';
 
 const marCal: MarCalType = new MarCal();
 
-const open: boolean = marCal.marketOpen();
-if (open) {
-  console.log('US market is open')
+const isMarketOpen: boolean = marCal.marketOpen();
+const minutesUntilClose: number = marCal.minutesToClose();
+
+if (isMarketOpen) {
+  console.log(`US market is open with ${minutesUntilClose} minutes until close`);
 } else {
-  console.log('US market is closed')
+  if (marCal.preMarket()) {
+    console.log('US market is in pre-market trading hours (4:00 AM - 9:30 AM ET)');
+  } else if (marCal.afterMarket()) {
+    console.log('US market is in after-hours trading (4:00 PM - 8:00 PM ET)');
+  } else {
+    console.log('US market is closed');
+  }
 }
